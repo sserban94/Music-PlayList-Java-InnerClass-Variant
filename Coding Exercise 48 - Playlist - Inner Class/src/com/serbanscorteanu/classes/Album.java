5556feadf63a519c52195578ef5002540b988c67
Serban Scorteanu <52545964+sserban94@users.noreply.github.com>
2021-08-11 23:25:46 +0300

    First commit

---
+++ Coding Exercise 48 - Playlist - Inner Class/src/com/serbanscorteanu/classes/Album.java
@@ -0,0 +1,102 @@
+package com.serbanscorteanu.classes;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+	private String name;
+	private String artist;
+	private SongList songs;
+//	private ArrayList<Song> songs;
+
+	public Album(String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+		songs = new SongList();
+		// SongList.songs = new ArrayList<>();
+	}
+
+	public boolean addSong(String title, double duration) {
+		// return true if added successfully
+		if (songs.findSong(title) != null) {
+			System.out.println("Error - Song already in playlist");
+			return false;
+		}
+		Song song = new Song(title, duration);
+		songs.songs.add(song);
+		return true;
+	}
+
+//	private Song findSong(String title) {
+//		// return song if found else null
+//		for (Song song : songs.songs) {
+//			if (song.getTitle().equals(title)) {
+//				return song;
+//			}
+//		}
+//		return null;
+//	}
+
+	public boolean addToPlayList(int trackNo, LinkedList<Song> playlist) {
+		// return true if it exists and successfully added else false
+		// add the specific song from the album to this playlist
+		if (!this.songs.songs.isEmpty()) {
+			if (trackNo > 0 && trackNo <= songs.songs.size()) {
+				Song song = songs.songs.get(trackNo - 1);
+				playlist.add(song);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+		// return true if it exists and successfully added using the title else false
+		if (!this.songs.songs.isEmpty()) {
+			Song song = songs.findSong(title);
+			if (song != null) {
+				playlist.add(song);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static class SongList {
+		private ArrayList<Song> songs;
+
+		private SongList() {
+			this.songs = new ArrayList<>();
+		}
+
+		private boolean add(Song song) {
+			// false if already in list
+			// true if not and success
+			Song retrievedSong = findSong(song.getTitle());
+			if (retrievedSong != null) {
+				System.out.println("Song already in list");
+				return false;
+			}
+			songs.add(retrievedSong);
+			return true;
+		}
+
+		private Song findSong(String title) {
+			// if found retrieve Song else null
+			for (Song song : songs) {
+				if (song.getTitle().equals(title)) {
+					return song;
+				}
+			}
+			return null;
+		}
+
+		private Song findSong(int trackNo) {
+			// if found retrieve Song else null
+			if (trackNo > 0 && trackNo <= songs.size()) {
+				return songs.get(trackNo - 1);
+			}
+			return null;
+		}
+	}
+}
---
+++ Coding Exercise 48 - Playlist - Inner Class/src/com/serbanscorteanu/main/Main.java
@@ -0,0 +1,232 @@
+package com.serbanscorteanu.main;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+import com.serbanscorteanu.classes.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Album> albums = new ArrayList<>();
+
+		Album album = new Album("Nightflight to Venus", "Boney M");
+		album.addSong("Nightflight to Venus", 4.46);
+		album.addSong("Rasputin", 5.57);
+		album.addSong("Painter Man", 3.1);
+		album.addSong("He Was a Steppenwolf", 6.51);
+		album.addSong("King of the Road", 2.35);
+		album.addSong("Rivers of Babylon", 4.18);
+		album.addSong("Voodoonight", 3.31);
+		album.addSong("Brown Girl in the Ring", 4.02);
+		album.addSong("Never Change Lovers in the Middle of the Night", 5.32);
+		album.addSong("Heart of Gold", 4.00);
+		albums.add(album);
+
+		album = new Album("The Best of ABBA", "ABBA");
+		album.addSong("Waterloo", 2.41);
+		album.addSong("Ring Ring", 3.01);
+		album.addSong("Honey, Honey", 2.52);
+		album.addSong("Mama Mia", 3.32);
+		album.addSong("People Need Love", 2.40);
+		album.addSong("Nina, Pretty Ballerina", 2.50);
+		album.addSong("I Do, I Do, I Do, I Do, I Do", 3.15);
+		album.addSong("SOS", 3.22);
+		album.addSong("Dance (While the Music Still Goes On)", 3.12);
+		album.addSong("Bang-A-Boomerang)", 3.00);
+		album.addSong("Hast Manana", 3.05);
+		album.addSong("So Long", 3.06);
+		albums.add(album);
+
+		LinkedList<Song> playlist = new LinkedList<>();
+		albums.get(0).addToPlayList("Rasputin", playlist);
+		albums.get(0).addToPlayList("Rivers of Babylon", playlist);
+
+		// the only one not in the album yet
+		System.out.println(albums.get(0).addToPlayList("Brown Girl in the Ring", playlist));
+
+		System.out.println(albums.get(0).addToPlayList(3, playlist));
+
+		System.out.println(albums.get(1).addToPlayList(1, playlist));
+		System.out.println(albums.get(1).addToPlayList(4, playlist));
+		System.out.println(albums.get(1).addToPlayList(13, playlist)); // no track with this number
+
+//		for (Song song : playlist) {
+//			System.out.println(song.toString());
+//		}
+
+		// Create menu of options - Quit, Skip forward, Skip backwards, Replay current,
+		// Remove current song from pl
+		// List songs in playlist
+		// Optional - remove current song from playList
+
+		printPlaylist(playlist);
+		System.out.println();
+		play(playlist);
+
+	}
+
+	public static void printPlaylist(LinkedList<Song> playList) {
+		Iterator<Song> playlistIterator = playList.iterator();
+		while (playlistIterator.hasNext()) {
+			Song song = playlistIterator.next();
+			System.out.println(song.toString());
+		}
+	}
+
+	public static void play(LinkedList<Song> playList) {
+		// need to get input from user => need scanner
+		Scanner scanner = new Scanner(System.in);
+		boolean quit = false; // use for the while loop - while quit is false show the menu
+
+		// this will keep track of the position of the iterator
+		// remember the iterator is always between elements
+		// REMEMBER LL Cycle Problem & Floyd's Tortoise and Hare Algorithm
+		boolean goingForward = true;
+
+		// create the iterator
+		ListIterator<Song> playlistIterator = playList.listIterator();
+
+		// check if there are any songs
+		if (playList.isEmpty()) {
+			System.out.println("Error. No songs in the playlist");
+		} else {
+			System.out.println("Now playing " + playlistIterator.next()); // this will start playback
+			printMenu();
+		}
+
+		// start creating the actual functionality of the menu
+		while (!quit) {
+			while (true) {
+				// make sure the user isn't trying to force an error
+				if (scanner.hasNextInt()) {
+					int option = scanner.nextInt();
+					scanner.nextLine(); // avoid bugs by skipping line
+					switch (option) {
+					case 0:
+						System.out.println("Music playback is over");
+						quit = true;
+						break;
+					case 1:
+						if (!goingForward) { // check if playback went backwards the previous time
+							if (playlistIterator.hasNext()) { // if it's not the end of the playlist (cursor after last
+																// elem)
+								playlistIterator.next(); // skip one element
+								goingForward = true;
+							}
+						}
+						if (playlistIterator.hasNext()) {
+							System.out.println("Now playing " + playlistIterator.next().getTitle());
+						} else {
+							System.out.println("Reached the end of the playlist");
+							goingForward = false; // if left true it would skip 1 when going back
+						}
+						break;
+					case 2:
+						if (goingForward) { // check if playback went forward last time
+							if (playlistIterator.hasPrevious()) {
+								playlistIterator.previous();
+								goingForward = false;
+							}
+						}
+						if (playlistIterator.hasPrevious()) {
+							System.out.println("Now playing " + playlistIterator.previous().getTitle());
+						} else {
+							System.out.println("Reached the beginning of the playlist");
+							goingForward = true;
+						}
+						break;
+					case 3:
+						if (goingForward) {
+							if (playlistIterator.hasPrevious()) {
+								System.out.println("Now playing " + playlistIterator.previous().getTitle());
+								goingForward = false;
+							} else {
+								System.out.println("Now playing " + playlistIterator.next().getTitle());
+								goingForward = true;
+							}
+						}
+						else {
+							if (playlistIterator.hasNext()) {
+								System.out.println("Now playing " + playlistIterator.next().getTitle());
+								goingForward = true;
+							} else {
+								System.out.println("Now playing " + playlistIterator.previous().getTitle());
+								goingForward = false;
+							}
+						}
+						break;
+					case 4:
+							// this can be done in fewer lines of code
+//						if (goingForward) {
+//							if (playlistIterator.hasPrevious()) {
+//								System.out.println("Deleting: " + playlistIterator.previous().getTitle());
+//								playlistIterator.remove();
+//								System.out.println("Done");
+//								goingForward = false;
+//							} else {
+//								System.out.println("Deleting: " + playlistIterator.next().getTitle());
+//								playlistIterator.remove();
+//								goingForward = true;
+//							}
+//						}
+//						else {
+//							if (playlistIterator.hasNext()) {
+//								System.out.println("Deleting " + playlistIterator.next().getTitle());
+//								playlistIterator.remove();
+//								System.out.println("Done");
+//								goingForward = true;
+//							} else {
+//								System.out.println("Now playing " + playlistIterator.previous().getTitle());
+//								playlistIterator.remove();
+//								System.out.println("Done");
+//								goingForward = false;
+//							}
+//						}
+						
+						// better shorter version
+						playlistIterator.remove();
+						if (playlistIterator.hasNext()) {
+							System.out.println("Now playing " + playlistIterator.next());
+							//goingForward = true;	// why does everything work ok without this?
+						} else if (playlistIterator.hasPrevious()) {
+							System.out.println("Now playing " + playlistIterator.previous());
+							//goingForward = false;
+						}
+						
+						break;
+					case 5:
+						System.out.println("Songs in playlist: ");
+						printPlaylist(playList);
+						break;
+					case 6:
+						printMenu();
+						break;
+					}
+				} else {
+					scanner.nextLine();
+					System.out.println("Only digits from 0 - 6 accepted");
+					continue;
+				}
+			}
+		}
+
+	}
+
+	public static void printMenu() {
+		System.out.println("Available actions:");
+		System.out.println("press ");
+		System.out.println("0 - to quit");
+		System.out.println("1 - to skip forward");
+		System.out.println("2 - to skip backwards");
+		System.out.println("3 - to replay current song");
+		System.out.println("4 - to remove current song");
+		System.out.println("5 - to show playlist");
+		System.out.println("6 - to show menu");
+	}
+
+}
