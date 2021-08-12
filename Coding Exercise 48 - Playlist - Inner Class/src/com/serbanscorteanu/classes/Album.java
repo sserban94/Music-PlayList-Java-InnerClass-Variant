f1e654f1b28bb655f11da2a46b36349ea3ab61a3
Serban Scorteanu <52545964+sserban94@users.noreply.github.com>
2021-08-12 09:23:31 +0300

    Update Album.java
    2nd commit - changed methods in album so they will call the similar methods from the inner class - encapsulation complete

--- Coding Exercise 48 - Playlist - Inner Class/src/com/serbanscorteanu/classes/Album.java
+++ Coding Exercise 48 - Playlist - Inner Class/src/com/serbanscorteanu/classes/Album.java
@@ -18,13 +18,15 @@ public class Album {
 
 	public boolean addSong(String title, double duration) {
 		// return true if added successfully
-		if (songs.findSong(title) != null) {
-			System.out.println("Error - Song already in playlist");
-			return false;
-		}
-		Song song = new Song(title, duration);
-		songs.songs.add(song);
-		return true;
+//		if (songs.findSong(title) != null) {
+//			System.out.println("Error - Song already in playlist");
+//			return false;
+//		}
+//		Song song = new Song(title, duration);
+//		songs.songs.add(song);
+//		return true;
+		
+		return this.songs.add(new Song(title, duration));
 	}
 
 //	private Song findSong(String title) {
@@ -40,28 +42,43 @@ public class Album {
 	public boolean addToPlayList(int trackNo, LinkedList<Song> playlist) {
 		// return true if it exists and successfully added else false
 		// add the specific song from the album to this playlist
-		if (!this.songs.songs.isEmpty()) {
-			if (trackNo > 0 && trackNo <= songs.songs.size()) {
-				Song song = songs.songs.get(trackNo - 1);
-				playlist.add(song);
-				return true;
-			}
-		}
+//		if (!this.songs.songs.isEmpty()) {
+//			if (trackNo > 0 && trackNo <= songs.songs.size()) {
+//				Song song = songs.songs.get(trackNo - 1);
+//				playlist.add(song);
+//				return true;
+//			}
+//		}
+//		return false;
+		
+		Song checkedSong = this.songs.findSong(trackNo);
+		if (checkedSong != null) {
+			playlist.add(checkedSong);
+			return true;
+		} 
 		return false;
 	}
 
 	public boolean addToPlayList(String title, LinkedList<Song> playlist) {
 		// return true if it exists and successfully added using the title else false
-		if (!this.songs.songs.isEmpty()) {
-			Song song = songs.findSong(title);
-			if (song != null) {
-				playlist.add(song);
-				return true;
-			}
+//		if (!this.songs.songs.isEmpty()) {
+//			Song song = songs.findSong(title);
+//			if (song != null) {
+//				playlist.add(song);
+//				return true;
+//			}
+//		}
+//		return false;
+		
+		Song checkedSong = this.songs.findSong(title);
+		if (checkedSong != null) {
+			playlist.add(checkedSong);
+			return true;
 		}
 		return false;
 	}
 
+	// inner class variant
 	public static class SongList {
 		private ArrayList<Song> songs;
 
@@ -72,12 +89,14 @@ public class Album {
 		private boolean add(Song song) {
 			// false if already in list
 			// true if not and success
-			Song retrievedSong = findSong(song.getTitle());
-			if (retrievedSong != null) {
+//			Song retrievedSong = findSong(song.getTitle());
+//			if (retrievedSong != null) {	
+			if (songs.contains(song)) {
 				System.out.println("Song already in list");
 				return false;
 			}
-			songs.add(retrievedSong);
+			//songs.add(retrievedSong);
+			songs.add(song);
 			return true;
 		}
 
